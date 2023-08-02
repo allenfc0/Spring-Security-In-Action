e7e93a47971f36cd6eb6af5f619783e84fdf47fa
Allen Farias <allenfarias29@gmail.com>
2023-08-01 22:04:46 -0700

    inserting data into tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO authorities VALUES (1, 'john', 'USER');
+-- INSERT IGNORE INTO user VALUES ('john', '12345', 'USER');
+-- INSERT IGNORE INTO user VALUES ('allen', '$2y$04$aESqLLTk/vwQXpUDNSVl8ueaORTNQ.wi3DxIShJrJWdWNzV.PtmQy', 'USER');
